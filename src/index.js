0c67d20bb6d0617becda8d2f5171a5620f556461
Altynay <altynaygoha2007@mail.ru>
2022-06-23 12:41:10 +0600

    feat: task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,60 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const singleToTeen = [
+        " ",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+
+    const doubleDigit = [
+        " ",
+        " ",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    let strings = number.toString();
+
+    if (number === 0) return "zero";
+    if (number < 20) return singleToTeen[number];
+    if (strings.length === 2) return doubleDigit[strings[0]] + " " + singleToTeen[strings[1]];
+    if (strings.length === 3) {
+        if (strings[1] === "0" && strings[2] === "0") {
+            return singleToTeen[strings[0]] + " hundred"
+        // } else if (strings[2] === "0") {
+        //     return (
+        //         singleToTeen[strings[0]] +
+        //         " hundred " +
+        //         doubleDigit[strings[1]]
+        //     );
+            } else {
+            return (
+                singleToTeen[strings[0]] +
+                " hundred " +
+                toReadable(+(strings[1] + strings[2]))
+            );
+        }
+    }
 }
